576ef137243045a2480cf5cb16c8ae694620d9c2
nourzakhama2003 <nourzakhma@gmail.com>
2026-05-30 14:29:24 +0100

    fix

--- restaurant/restaurant-frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    keycloak: {
-        url: 'http://localhost:8080',
-        realm: 'myrealm',
-        clientId: 'myclient'
-    },
-    apiUrl: 'http://localhost:8081/api'
-};
